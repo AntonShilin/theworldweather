1ba0ec3ac589c70f1e8ac5fcaffffd547e919359
anton <anton1002@yahoo.com>
2022-04-27 13:21:02 +0300

    add content todays forecast component

--- src/Componnents/TodaysForecast/TodaysForecast.jsx
+++ src/Componnents/TodaysForecast/TodaysForecast.jsx
@@ -17,23 +17,33 @@ class TodaysForecast extends Component {
         </header>
         <div className={style.todays_forecast_main}>
           <ul>
-            <li>
-              <Link to="#">
-                <h3>Morning</h3>
-                <div>
-                  <span>7 &#176;</span>
-                </div>
-                <div>
-                  <img
-                    src={`http://openweathermap.org/img/wn/${this.props.dailyWeather.weather[0].icon}@2x.png`}
-                    alt="img"
-                  />
-                </div>
-                <div>
-                  <span>10% rain</span>
-                </div>
-              </Link>
-            </li>
+            {Object.keys(this.props.dailyWeather.feels_like).map(
+              (timeOfDay, i) => (
+                <li key={i}>
+                  <Link to="#">
+                    <h3>{timeOfDay}</h3>
+                    <div>
+                      <span>
+                        {this.props.dailyWeather.temp[timeOfDay]} &#176;
+                      </span>
+                    </div>
+                    <div>
+                      <img
+                        src={`http://openweathermap.org/img/wn/${this.props.dailyWeather.weather[0].icon}@2x.png`}
+                        alt="img"
+                      />
+                    </div>
+                    <div>
+                      {this.props.dailyWeather.rain ? (
+                        <span>Rain {this.props.dailyWeather.rain}%</span>
+                      ) : (
+                        <span>---</span>
+                      )}
+                    </div>
+                  </Link>
+                </li>
+              )
+            )}
           </ul>
         </div>
         <footer>
